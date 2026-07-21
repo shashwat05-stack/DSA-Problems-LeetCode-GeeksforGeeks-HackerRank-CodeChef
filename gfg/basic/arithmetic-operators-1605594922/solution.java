4f87e75948969df8d660deef2ebd358e49be13bc
Shashwat Gupta <shashwatgupta205@gmail.com>
2026-07-22 00:11:55 +0530

    ✅ Arithmetic Operators | Basic | Java | Runtime: N/A | Memory: N/A | 2026-07-21

---
+++ gfg/basic/arithmetic-operators-1605594922/solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        // code here
+        int p = x + y;
+        int q = x - y;
+        int r = x * y;
+        int s = x / y;
+        int t = x % y;
+
+        // The below code prints the output
+        System.out.println(p + " " + q + " " + r + " " + s + " " + t);
+    }
+}
