74a2286e15fcd6f30cf36f01f052ccc6b728e89a
Shashwat Gupta <shashwatgupta205@gmail.com>
2026-07-23 00:52:55 +0530

    ✅ Type Conversion | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-22

---
+++ gfg/easy/type-conversion--151956/solution.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double d = sc.nextDouble();
+
+        // code here
+        int i = (int)d;
+        
+        System.out.println(i);
+    }
+}
