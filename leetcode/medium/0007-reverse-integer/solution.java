3beb5e8364c7ef9f721f356aa126fc6babaff1d8
Shashwat Gupta <shashwatgupta205@gmail.com>
2026-07-23 23:06:30 +0530

    ✅ Reverse Integer | Medium | Java | Runtime: 0 ms | Memory: 42.2 MB | 2026-07-23

--- leetcode/medium/0007-reverse-integer/solution.java
+++ leetcode/medium/0007-reverse-integer/solution.java
@@ -3,12 +3,13 @@ class Solution {
         int reverse = 0;
 
         while(x != 0){
-            x = x/10;
             int digit = x % 10;
-            
             reverse = reverse * 10 + digit;
-            
+            x = x/10;
         }
-        return reverse;
+         if (reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE) {
+            return 0;
+         }
+        return (int) reverse;
     }
         }
