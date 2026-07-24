c120696768810b991c170bb6f3ffecf3716e5794
Shashwat Gupta <shashwatgupta205@gmail.com>
2026-07-24 12:50:25 +0530

    ✅ SEARCHINARR - Rating 600 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-24

---
+++ codechef/easy/SEARCHINARR---Rating-600/solution.java
@@ -0,0 +1,10 @@
+public static String solve(int N, int X, int[] A) {
+
+    for (int i = 0; i < N; i++) {
+        if (A[i] == X) {
+            return "YES";
+        }
+    }
+
+    return "NO";
+}
