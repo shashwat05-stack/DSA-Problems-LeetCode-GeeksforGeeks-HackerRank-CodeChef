e9ab929d09325711052d95d303b176692c9b7db7
Shashwat Gupta <shashwatgupta205@gmail.com>
2026-07-27 12:31:27 +0530

    ✅ Function With No Arguments | Basic | Java | Runtime: N/A | Memory: N/A | 2026-07-27

---
+++ gfg/basic/function-with-no-arguments-1605703188/solution.java
@@ -0,0 +1,13 @@
+// Write the helloFunction below to print "Hello"
+
+class Solution {
+    // code here
+    
+    static void helloFunction(){
+        System.out.println("Hello");
+    }
+    
+    public static void main(String args[]){
+        helloFunction();
+    }
+}
