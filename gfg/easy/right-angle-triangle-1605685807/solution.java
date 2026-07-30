db40cc6a440aa84c578885c974bdf179c17f6231
Shashwat Gupta <shashwatgupta205@gmail.com>
2026-07-30 15:02:01 +0530

    ✅ Right Angle Triangle Pattern | Easy | Java | Runtime: N/A | Memory: N/A | 2026-07-30

--- gfg/easy/right-angle-triangle-1605685807/solution.java
+++ gfg/easy/right-angle-triangle-1605685807/solution.java
@@ -7,7 +7,7 @@ class GFG {
 
         // code here
         for(int i=0;i<n;i++){
-            for(int j=0;j<i+1;j++){
+            for(int j=0;j<=i;j++){
                 System.out.print("* ");
             }
             System.out.println();
