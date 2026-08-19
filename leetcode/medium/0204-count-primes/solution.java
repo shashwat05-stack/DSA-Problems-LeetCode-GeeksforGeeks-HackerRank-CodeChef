414699f1fdad7c03597bece1bdd314c46b3f753f
Shashwat Gupta <shashwatgupta205@gmail.com>
2026-08-19 13:32:37 +0530

    ✅ Count Primes | Medium | Java | Runtime: 0 ms | Memory: 41.9 MB | 2026-08-19

---
+++ leetcode/medium/0204-count-primes/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countPrimes(int n) {
+        int count = 0;
+        for(int i = 2; i < n; i++){
+            boolean isPrime = true;
+
+            for(int j = 2; j*j <= i; j++) {
+                if(i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
