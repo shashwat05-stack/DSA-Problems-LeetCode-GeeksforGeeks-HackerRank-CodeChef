fb93f43230aa3f846a631bb13f2f9984018e4369
Shashwat Gupta <shashwatgupta205@gmail.com>
2026-08-20 00:54:35 +0530

    ✅ Greatest Common Divisor of Strings | Easy | Java | Runtime: 0 ms | Memory: 42.7 MB | 2026-08-19

--- leetcode/easy/1071-greatest-common-divisor-of-strings/solution.java
+++ leetcode/easy/1071-greatest-common-divisor-of-strings/solution.java
@@ -1,6 +1,6 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        if ((str1 + str2).equals(str2 + str1)) {
+        if (!(str1 + str2).equals(str2 + str1)) {
             return "";
         }
         int a = str1.length();
